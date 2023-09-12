42c5584a6247f6ee2660c8dadf8d0148c999e5bb
Blake Appleby <aba2176@columbia.edu>
2023-09-12 16:03:14 -0400

    elisp baby steps

---
+++ typing/caps.el
@@ -0,0 +1,35 @@
+;;; caps.el --- Generates strings useful for improving typing speed -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Blake
+;;
+;; Author: Blake <blake@blake-laptop>
+;; Maintainer: Blake <blake@blake-laptop>
+;; Created: September 12, 2023
+;; Modified: September 12, 2023
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/blake/caps
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(provide 'caps)
+
+;; Blake's Elisp notes
+;; hello world - t denotes the echo area as the output
+;; (print "Hello world" t)
+;;
+;; get / create an empty buffer - evals to buffer
+;; (get-buffer-create "output")
+;;
+;; Create a new buffer
+;; (generate-new-buffer "output")
+;;
+;; print out to a buffer. Fix these quotes somehow?
+;; (print "Hello world" (generate-new-buffer "output"))
